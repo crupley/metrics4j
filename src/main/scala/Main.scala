55c6f65f68bf110d79547a676fea419f90d9b06f
Chris Rupley <crupley@salesforce.com>
2018-08-03 12:39:26 -0700

    use Configurator instead of .properties

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,32 @@
 
-import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.core.config.Configurator
+import org.apache.logging.log4j.{Level, LogManager, Logger}
 import org.apache.logging.log4j.scala.Logging
 
 
 object Main extends Logging {
 
+  // val log: Logger = LogManager.getRootLogger
+  Configurator.setRootLevel(Level.ALL)
+
   def doStuff(): Unit = {
     logger.info("Doing stuff")
   }
   def doStuffWithLevel(level: Level): Unit = {
+    print(s"Trying log level: ${level.name}\t:  ")
     logger(level, "Doing stuff with arbitrary level")
   }
 
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
-    doStuff()
+    doStuffWithLevel(Level.ALL)
     doStuffWithLevel(Level.INFO)
+    doStuffWithLevel(Level.DEBUG)
     doStuffWithLevel(Level.WARN)
     doStuffWithLevel(Level.ERROR)
-    doStuffWithLevel(Level.ALL)
+    doStuffWithLevel(Level.FATAL)
     doStuffWithLevel(Level.TRACE)
+    doStuffWithLevel(Level.OFF)
   }
 }
